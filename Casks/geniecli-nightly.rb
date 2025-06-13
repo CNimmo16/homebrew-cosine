bdf7850f0a554cf8a1643cdc693e6ba906f47303
CNimmo16 <48329714+CNimmo16@users.noreply.github.com>
2025-06-13 17:55:52 +0100

    add nightly cask

---
+++ Casks/geniecli-nightly.rb
@@ -0,0 +1,26 @@
+cask "geniecli-nightly" do
+  arch arm: "arm64", intel: "x86_64"
+  version :latest
+
+  on_intel do
+    url "https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-amd64.zip"
+    sha256 do
+      `curl -s https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-amd64.zip.sha256`.strip
+    end  
+  end
+  on_arm do
+    url "https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-arm64.zip"
+    sha256 do
+      `curl -s https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-arm64.zip.sha256`.strip
+    end
+  end
+
+  name "Genie CLI Nightly"
+  desc "Nightly builds of the Genie CLI"
+
+  binary "cosine"
+
+  preflight do
+    system_command "ls", args: ["-la", staged_path]
+  end  
+end
