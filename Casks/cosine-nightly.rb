99fe134382f248172f33131fd09369f5a83bae09
CNimmo16 <48329714+CNimmo16@users.noreply.github.com>
2025-06-20 11:53:00 +0100

    remove livecheck

--- Casks/cosine-nightly.rb
+++ Casks/cosine-nightly.rb
@@ -1,6 +1,6 @@
 cask "cosine-nightly" do
   arch arm: "arm64", intel: "x86_64"
-  version :latest
+  version "nightly-0"
 
   on_intel do
     url "https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-amd64.zip"
@@ -9,10 +9,6 @@ cask "cosine-nightly" do
     url "https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-arm64.zip"
   end
   sha256 :no_check
-
-  livecheck do
-    url "https://github.com/CNimmo16/genie-cli/releases/download/nightly/cosine-darwin-arm64.zip.sha256"
-  end
 
   name "Cosine CLI Nightly"
   desc "Nightly builds of the Cosine CLI"
